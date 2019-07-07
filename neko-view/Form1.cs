6aecdcebf63194ce9c60e5b10b391ad87708f91f
ttakkku <k2005118@gmail.com>
2019-07-07 23:16:00 +0900

    Neko view Now

---
+++ neko-view/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace neko_view
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            using (WebClient client = new WebClient())
+            {
+                var neko = client.DownloadString("https://nekos.life/api/v2/img/neko");
+                JObject json = JObject.Parse(neko);
+                var nekourl = json["url"].ToString();
+                   pictureBox1.LoadAsync(nekourl);
+                //pictureBox1.ImageLocation = nekourl;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (WebClient client = new WebClient())
+            {
+                var neko = client.DownloadString("https://nekos.life/api/v2/img/neko");
+                JObject json = JObject.Parse(neko);
+                var nekourl = json["url"].ToString();
+                pictureBox1.LoadAsync(nekourl);
+                //pictureBox1.ImageLocation = nekourl;
+            }
+        }
+    }
+}
